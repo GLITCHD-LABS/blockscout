ea44b128a18481f25229975a162763aede10acb6
DukeCaesar <taoqinget@gmail.com>
2024-05-27 02:36:34 +0800

    feat: enable certbot

--- docker-compose/services/nginx/init.sh
+++ docker-compose/services/nginx/init.sh
@@ -6,7 +6,7 @@ set -xe
 nginx -g "daemon on;"
 # create a certificate
 # apply for the certificates separately as a way to avoid the request limit
-# certbot --nginx --non-interactive --agree-tos --keep-until-expiring --email "george@glitchd.network" -d explorer.glitchd.network --redirect --verbose
+certbot --nginx --non-interactive --agree-tos --keep-until-expiring --email "george@glitchd.network" -d explorer.glitchd.network --redirect --verbose
 # reload nginx
 nginx -s quit
 nginx -g "daemon off;"
