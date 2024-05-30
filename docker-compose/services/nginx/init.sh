b9340bf3456a040a16a504c8feebcc1da0798afa
DukeCaesar <taoqinget@gmail.com>
2024-05-31 00:01:36 +0800

    refactor: certbot has reached its weekly limits of 5 certs

--- docker-compose/services/nginx/init.sh
+++ docker-compose/services/nginx/init.sh
@@ -5,7 +5,7 @@ set -xe
 # start nginx in background
 nginx -g "daemon on;"
 # create a certificate
-certbot --nginx --non-interactive --agree-tos --keep-until-expiring --email "george@glitchd.network" -d explorer.glitchd.network -d explorer.lanmaodudu.com --no-redirect --verbose
+certbot --nginx --non-interactive --agree-tos --keep-until-expiring --email "george@glitchd.network" -d explorer.glitchd.network -d explorer.jimuzhaopin.com --no-redirect --verbose
 # reload nginx
 nginx -s quit
 nginx -g "daemon off;"
